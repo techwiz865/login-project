27c3bf13a0d5d9f841d0a53f54dfa9a564edc354
techwiz865 <techwiz865@gmail.com>
2025-02-20 19:12:12 +0000

    commiting now

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Add JS here
+submit.onclick = userSubmitted;
+
+var users = {user1: ["wombat_fire123", "bug"]};
+
+function userSubmitted() {
+    const rawusername = document.getElementById("username");
+    const rawpassword = document.getElementById("password");
+    const username = rawusername.value;
+    const password = rawpassword.value;
+    console.log(username);
+    console.log(password);
+}
+    
